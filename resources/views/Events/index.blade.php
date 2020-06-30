43f548333c58b17c0397287f45313ba189651776
motoshifurugen <amanogawa_m@icloud.com>
2020-06-30 15:04:41 +0900

    ビュー作成

---
+++ resources/views/Events/index.blade.php
@@ -0,0 +1,99 @@
+<style>
+* {
+    font-size: 0.9rem;
+}
+h4 {
+    padding: 0.25em 0.5em;
+    border-left: solid 5px blue;
+}
+th {
+    background-color: gray;
+    color: white;
+    font-weight: normal;
+}
+</style>
+
+<form action="/event">
+    <table border="1" cellspacing="0" cellpadding="5">
+        <tr align="left">
+            <th>期間</th>
+            <td>
+                <input type="date" name="date_from" value= @if(request('date_from')) {{ request('date_from') }} @endif />
+                &nbsp ~ &nbsp;
+                <input type="date" name="date_to" value= @if(request('date_to')) {{ request('date_to') }} @endif />
+            </td>
+        </tr>
+        <tr align="left">
+            <th>収支</th>
+            <td>
+                @foreach ($in_outs as $key => $val)
+                    <input type="radio" name="in_out" value="{{ $key }}" @if ((!request('in_out') && $key == 'ALL') || (request('in_out') == $key)) checked @endif />{{ $value }}
+                @endforeach
+            </td>
+        </tr>
+        <tr align="left">
+            <th>項目</th>
+            <td>
+                @foreach($categories as $key => $value)
+                    <input type="checkbox" name="category" value="{{ $key }}" @if(request('category') && in_array($key, request('category'))) checked @endif /> {{ $value }}
+                @endforeach
+            </td>
+        </tr>
+        <tr align="center">
+            <td colspan="2">
+                <button type="submit">検索</button>
+                <button type="button" onclick="location.href='/events'">リセット</button>
+            </td>
+        </tr>
+    </table>
+</form>
+
+<h4>統計</h4>
+<table border="1" cellspacing="0" cellpadding="5">
+    <tr align="center">
+        <th>収入合計</th>
+        <th>支出合計</th>
+        <th>収支合計</th>
+    </tr>
+    <tr align="right">
+        <td>{{ $pf['total_in'] }}</td>
+        <td>{{ $pf['total_out'] }}</td>
+        <td>{{ $pf['in_out_money'] }}</td>
+    </tr>
+</table>
+
+<h4>明細</h4>
+<button type="button" onclick="location.href='/events/create'">登録</button>
+<br />
+<br />
+<table border="1" cellspacing="0" cellpadding="5">
+    <tr align="center">
+        <th>日時</th>
+        <th>収支</th>
+        <th>項目</th>
+        <th>金額</th>
+        <th>メモ</th>
+        <th colspan="2">操作</th>
+    </tr>
+    @foreach($events as $ev)
+    <tr>
+        <td>{{ \Carbon\Carbon::create($ev->date)->format('Y/m/d h:i') }}</td>
+        <td align="center">{{ $ev->in_out }}</td>
+        <td align="center">{{ $ev->category }}</td>
+        <td align="right">{{ $ev->money }}円</td>
+        <td>{{ $ev->memo }}</td>
+        <td><button type="button" onclick="location.href='event/{{ $ev->id }}/edit'">編集</button></td>
+        <td valign="middle">
+            <form action="/event/{{ $ev->id }}" method="POST" style="display: inline">
+                @csrf
+                @method('DELETE')
+                <button type="submit">削除</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>
+
+
+
+
---
+++ resources/views/Events/save.blade.php
@@ -0,0 +1,70 @@
+<style>
+*{
+    font-size: 0.9rem;
+}
+h4 {
+    padding: 0.25em 0.5em;
+    border-left: solid 5px blue;
+}
+th {
+    background-color: gray;
+    color: white;
+    font-weight: normal;
+}
+</style>
+
+@if($type == 'create')
+<form method="POST" action="/event">
+@else
+<form method="POST" action="/event/{{ $ev->id }}">
+    @method('PUT')
+@endif
+
+@csrf
+<table border="1" cellspacing="0" cellpadding="5">
+    <tr>
+        <th>日時</th>
+        <td><input type="datetime-local" name="date" value="{{ $ev->date }}" required ></td>
+    </tr>
+    <tr>
+        <th>収支</th>
+        <td>
+            @foreach ($in_outs as $key => $value)
+                <input type="radio" name="in_out" value="{{ $key }}" @if ($ev->in_out == $key) checked @endif />{{ $val }}
+            @endforeach
+        </td>
+    </tr>
+    <tr>
+        <th>項目</th>
+        <td>
+            <select name="category">
+                @foreach ($categories as $key => $val)
+                    <onption value="{{ $key }}" @if ($ev->category == $key) checked @endif >{{ $val }}</onption>
+                @endforeach
+            </select>
+        </td>
+    </tr>
+    <tr>
+        <th>金額</th>
+        <td><input type="number" name="money" value="{{ $ev->money }}" required></td>
+    </tr>
+    <tr>
+        <th>メモ</th>
+        <td><textarea name="memo" value="{{ $th->memo }}"></textarea></td>
+    </tr>
+    <tr align="center">
+        <td colspan="2"><button type="submit">{{ $type == 'create' ? '登録' : '更新' }}</button></td>
+    </tr>
+</table>
+</form>
+
+@if ($errors->any())
+<ul>
+    @foreach($errors->all() as $error)
+        <li style="color:red">{{ $error }}</li>
+    @endforeach
+</ul>
+@endif
+
+<a href="/event">戻る</a>
+
