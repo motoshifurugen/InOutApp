251c00007bf57671ff190e3bc1fbc989ea3c2c8e
motoshifurugen <amanogawa_m@icloud.com>
2020-07-05 11:41:45 +0900

    seeder

--- database/seeds/BalancesSeeder.php
+++ database/seeds/BalancesSeeder.php
@@ -13,8 +13,11 @@ class BalancesSeeder extends Seeder
     {
         DB::table('balances')->insert([
             ['user_id'=> 1, 'transaction_date' => now(), 'category_id' => '0', 'amount' => 10000, 'memo' => '入金' ],
-            ['user_id'=> 1, 'transaction_date' => now(), 'category_id' => '1', 'amount' => -2000, 'memo' => '購入' ],
-            ['user_id'=> 1, 'transaction_date' => now(), 'category_id' => '2', 'amount' => -1000, 'memo' => '購入' ]
+            ['user_id'=> 2, 'transaction_date' => now(), 'category_id' => '1', 'amount' => -2000, 'memo' => '購入' ],
+            ['user_id'=> 3, 'transaction_date' => now(), 'category_id' => '2', 'amount' => -1000, 'memo' => '購入' ],
+            ['user_id'=> 4, 'transaction_date' => now(), 'category_id' => '2', 'amount' => -1000, 'memo' => '購入' ],
+            ['user_id'=> 5, 'transaction_date' => now(), 'category_id' => '2', 'amount' => -1000, 'memo' => '購入' ],
+            ['user_id'=> 6, 'transaction_date' => now(), 'category_id' => '2', 'amount' => -1000, 'memo' => '購入' ]
         ]);
     }
 }
